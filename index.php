7fc27653ea54f926829ae4ec46ecf366115a70e0
Maryfrances Umeora <mumeora@u.rochester.edu>
2021-10-07 04:08:46 -0500

    home page formatting

--- index.php
+++ index.php
@@ -1,76 +1,61 @@
+<?php
+    //include '/home/mumeora/dbconfig.php';
+    include 'scripts/dbconfig.php';
+?>
+
 <!DOCTYPE html>
-<html lang="">
+<html>
 
   <head>
     <meta charset="utf-8">
     <title> The Raven Desk </title>
-
-    <style>
-    body {
-        background-image: url("style/img/background.png");
-    }
-    
-    #logo{
-        position: fixed;
-        width: 10%;
-    }
-    
-    h1{
-        position: fixed;
-        margin-left: 12%;
-        margin-top: 2%;
-        color: white;
-        font-size: 60px;
-    }
-
-    #bgvideo {
-      position: fixed;
-      right: 0;
-      bottom: 0;
-      min-width: 100%;
-      min-height: 100%;
-    }
- 
-    #loginBox {
-      position: fixed;
-      width: 20%;
-      height: 60%;
-      margin-top: 8%;
-      margin-left: 70%;
-      background: #d9d8d9;
-      color: black;
-      padding: 2%;
-      border-radius: 1.5em;
-      box-shadow: 0px 11px 35px 2px rgb(0, 0, 0);
-    }
-
-    form {
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-    }
-    </style>
+    <link rel="stylesheet" href="style/main.css"/>
   </head>
 
 
   <body>
-    
-    <img id="logo" src="style/img/logo.png">
-    
-    <h1> The Raven Desk </h1>
 
-    <div id="loginBox">
-      <form>
-        <h3> Begin </h3>
-        <label for="email"> Email </label>
-        <input type="email" name="email">
-        <label for="pass">Password</label>
-        <input type="password" name="pass">
-
-        <button type="submit"> Log In </button>
-        <a href="#"> Don't Have an Account? </a>
-      </form>
+    <div class="header-row">
+        <img id="logo" src="style/img/logo.png">
+        <h1> The Raven Desk </h1>
     </div>
+
+    <div class="index-container">
+
+        <div class="row">
+            <div class="formbox">
+                <form method="post" action="createuser.inc.php">
+                    <h3> Sign Up </h3>
+                    <!-- <label for="email"> Email </label> -->
+                    <input type="email" name="emailSignup" required placeholder="Email"> 
+
+                    <!-- <label for="pass">Password</label> -->
+                    <input type="password" name="passSignup" required placeholder="Password"> 
+
+                    <!-- <label for="conf">Confirm Password</label> -->
+                    <input type="password" name="confSignup" required placeholder="Confirm Password"> 
+
+                    <button type="submit" name="submitSignup"> Sign Up </button>
+                </form>
+            </div>
+
+            <div class="formbox">
+                <form method="post" action="loginuser.inc.php">
+                    <h3> Log In </h3>
+                    <!-- <label for="loginEmail"> Email </label> -->
+                    <input type="email" name="loginEmail" required placeholder="Email">
+
+                    <!-- <label for="loginPass">Password</label> -->
+                    <input type="password" name="loginPass" required placeholder="Password">
+
+                    <button type="submit" name="submit2"> Log In </button>
+                </form>
+                
+            </div>
+
+        </div> <!-- end of row class-->
+
+    </div> <!-- end of index container -->
 
   </body>
 </html>
--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,1 +1,5 @@
- 
+<head>
+    <meta charset="utf-8">
+    <title> The Raven Desk </title>
+    <link rel="stylesheet" href="style/main.css"/>
+  </head> 
