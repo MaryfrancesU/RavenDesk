4112a04dcc57016c6a63a470a846e1fa30c7a187
Maryfrances Umeora <mumeora@u.rochester.edu>
2021-10-08 23:11:41 -0500

    dashboard page frontend

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -2,11 +2,8 @@
     //include '/home/mumeora/dbconfig.php';
     include '../scripts/dbconfig.php';
 
-    if (isset($_SESSION["userid"])){
-			echo "<p> User with id ".$_SESSION["userid"]." is logged in.";
-		}
-    else {
-      header("location:../index.php?error=notloggedin");
+    if (!isset($_SESSION["userid"])){
+        header("location:../index.php?error=notloggedin");
     }
 
 ?>
@@ -14,22 +11,32 @@
 
 <!DOCTYPE html>
 <html>
+    
+    <head>
+        <meta charset="utf-8">
+        <title> The Raven Desk </title>
+        <link rel="stylesheet" href="../style/main.css"/>
+    </head>
 
-  <head>
-    <meta charset="utf-8">
-    <title> The Raven Desk </title>
-    <link rel="stylesheet" href="../style/main.css"/>
-  </head>
+    <body>
+        <div class="nav-row">
+            <a href="../scripts/auth/logoutuser.inc.php"> Logout </a>
+            <div class="header-row">
+                <img id="logo" src="../style/img/logo.png">
+                <h1> Projects </h1>
+            </div>
+        </div>
 
-  <style>
-    p{
-      color: white;
-    }
-  </style>
-
-  <body>
-    <a href="../scripts/auth/logoutuser.inc.php"> Logout </a>
-  </body>
+        <div class="projects-grid-container">
+            <div id="item1"> <a href="#">+</a> </div>
+            <div> That One Time I Got Reincarnated As A Slime </div>
+            <div> Title </div>
+            <div> Title </div>
+            <div> Title </div>
+        </div>
+    
+        
+    </body>
 
 
 </html>
